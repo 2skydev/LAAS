01226e7f28e94070a8ecf150771de784cf4cf982
2skydev <2skydev@gmail.com>
2022-08-17 17:40:27 +0900

    버그 수정

--- app/stores/update.ts
+++ app/stores/update.ts
@@ -1,10 +1,8 @@
-import { app } from 'electron';
 import Store from 'electron-store';
 
 import { UpdateStatus } from '@app/utils/updater';
 
 export interface UpdateStoreValues {
-  version: string;
   status: UpdateStatus;
 }
 
@@ -12,7 +10,6 @@ export const updateStore = new Store<UpdateStoreValues>({
   name: 'update',
   accessPropertiesByDotNotation: false,
   defaults: {
-    version: app.getVersion(),
     status: {
       event: 'checking-for-update',
       data: null,
--- src/stores/update.ts
+++ src/stores/update.ts
@@ -1,7 +1,7 @@
 import { UpdateStoreValues } from '@app/stores/update';
 import { atom } from 'recoil';
 
-export const updateStore = atom<UpdateStoreValues>({
+export const updateStore = atom<UpdateStoreValues & { version: string }>({
   key: 'update',
   default: (async () => {
     return {
