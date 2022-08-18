28f1bd19c3c4ea2567f2e7ea958978681de03da5
2skydev <2skydev@gmail.com>
2022-08-18 09:46:33 +0900

    dayjs 한국어 설정 및 relativeTime 추가

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,15 @@ import { HashRouter } from 'react-router-dom';
 
 import { ElectronRendererContext } from '@app/preload';
 import 'antd/dist/antd.css';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import { RecoilRoot } from 'recoil';
 
 import FileSystemRoutes from '~/components/FileSystemRoutes';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
 
 declare global {
   interface Window {
